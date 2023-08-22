ae9f49df58ff3200bac0a13e6c8652109042d120
Yulia Dobrova <dobrova2009@gmail.com>
2023-08-21 19:54:10 -0700

    app styles fix

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,17 +1,17 @@
-import { TailSpin } from  'react-loader-spinner'
+import { TailSpin } from "react-loader-spinner";
 
 const LoadingSpinner = () => {
-  return(
+  return (
     <TailSpin
-    height="160"
-    width="160"
-    color="#4fa94d"
-    ariaLabel="tail-spin-loading"
-    radius="2"
-    wrapperStyle={{}}
-    wrapperClass=""
-    visible={true}
-  />
+      height="100"
+      width="100"
+      color="#174ea6"
+      ariaLabel="tail-spin-loading"
+      radius="2"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
   );
-}
+};
 export default LoadingSpinner;
